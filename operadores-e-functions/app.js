b80c6c224481a209b852cf3865a2b538f53b11d3
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2022-12-19 15:51:24 -0300

    bug
    resolvido bug

--- operadores-e-functions/app.js
+++ operadores-e-functions/app.js
@@ -5,11 +5,11 @@ const readline = require('readline').createInterface({
 });
 
 const validarnumeroinformado = (numero) => {
-    try{
-        return Number.parseFloat(numero);
-    }catch(e){
+   const resultado = Number.parseFloat(numero);
+   if(!resultado){
         console.log('Numero informado não é válido');
-    }
+   }
+   return resultado;
 }
 
 const validaroperador = (operador) => {
@@ -31,7 +31,7 @@ readline.question('Favor informar um número:', (numero1) => {
   const numerovalidado1 = validarnumeroinformado(numero1);
 
   if(numerovalidado1){
-    readline.question('Favor informar outro número:', (mumero2) =>{
+    readline.question('Favor informar outro número:', (numero2) =>{
         const numerovalidado2 = validarnumeroinformado(numero2);
 
         if(numerovalidado2){
