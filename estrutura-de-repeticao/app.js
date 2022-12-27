7daad438712435fc2cb75dd1b8c27d64f73acdf5
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2022-12-27 13:48:55 -0300

    controlador while
    estrutura de repetição while

--- estrutura-de-repeticao/app.js
+++ estrutura-de-repeticao/app.js
@@ -4,4 +4,16 @@ console.log('---------------executando um for--------------------');
 
 for(let controladorFor = 0; controladorFor < listaArgumentos.length; controladorFor++){
     console.log(`Posição ${controladorFor} valor lido = ${listaArgumentos[controladorFor]}`);
+    
+}
+console.log('-----------------terminado--------------------------');
+
+
+
+console.log('--------------executando um WHILE--------------------');
+
+let controladorWhile = 0;
+while(controladorWhile < listaArgumentos.length){
+    console.log(`Posição ${controladorWhile} valor lido = ${listaArgumentos[controladorWhile]}`);
+    controladorWhile++;
 }
