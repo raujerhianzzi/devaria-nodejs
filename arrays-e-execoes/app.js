cb580d77d6d7bede08379c85cb794dba79805384
raujerhianzzi <107506236+raujerhianzzi@users.noreply.github.com>
2022-12-28 17:03:22 -0300

    .

---
+++ arrays-e-execoes/app.js
@@ -0,0 +1,25 @@
+const listaDeProdutosDisponiveis =[
+    "Pão",
+    "café",
+    "laranja",
+    "macarrão",
+    "detergente",
+    "sabonete",
+];
+ const listaDeArgumentos = process.argv.slice(2);
+
+ const listaDeProdutosSolicitadosDisponiveis = listaDeProdutosDisponiveis.filter(produto => {
+    return listaDeArgumentos.find(argumento => argumento === produto);
+ })
+
+ listaDeProdutosSolicitadosDisponiveis.forEach(produto => console.log(` este produto nos temos: ${produto}`));
+
+ const listaDeProdutosSolicitadosNaoDisponiveis = listaDeArgumentos.filter(argumento => {
+    return !listaDeProdutosDisponiveis.find(produto => produto === argumento);
+ });
+
+ listaDeProdutosSolicitadosNaoDisponiveis.forEach(argumento => console.log(`Este produto nós não temos: ${argumento}`));
+
+ listaDeProdutosDisponiveis.sort();
+
+ listaDeProdutosDisponiveis.forEach(produto => console.log(`Este produto está disponivel: ${produto}`));
